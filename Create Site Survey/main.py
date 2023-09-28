036e803f072812d80e853a6a5607a4300af61339
Aeriq522 <eriksanchez6@gmail.com>
2023-09-27 23:44:40 -0700

    fixed for site surveys

--- Create Site Survey/main.py
+++ Create Site Survey/main.py
@@ -1,6 +1,7 @@
 import csv
 import os
 from datetime import datetime, timedelta
+import time
 # from dotenv import load_dotenv
 from playwright.sync_api import Playwright, sync_playwright, expect
 
@@ -62,6 +63,27 @@ def run(playwright: Playwright) -> None:
             Total_dids = row[15]
             Notes = row[16]
             Fieldnation_Workorder_Link = row[17]
+            
+            # Define a function to process a single row from the CSV file
+            def process_row(row):
+                Ticket_title = row[1]
+                Location = row[2]
+                Carrier = row[3]
+                Download = row[4]
+                Upload = row[5]
+                Mount = row[6]
+                Facp = row[7]
+                Primary = row[8]
+                Secondary = row[9]
+                Burglar = row[10]
+                Intercomm = row[11]
+                number_of_elevators = row[12]
+                Elevator = row[13]
+                Elevator_notes = row[14]
+                Fax = row[15]
+                Total_dids = row[16]
+                Notes = row[17]
+                # Extract other data from the row as needed
 
        # Processed data
             # number of elevators is a string, this converts it to an integer
@@ -71,127 +93,139 @@ def run(playwright: Playwright) -> None:
             browser = playwright.chromium.launch(headless=False)
             context = browser.new_context()
             page = context.new_page()
-            page.goto(
+            
+            try:
+                page.goto(
                 "https://machnetworks.zendesk.com/agent/filters/360205088072")
-            page.goto("https://machnetworks.zendesk.com/access/unauthenticated?return_to=https%3A%2F%2Fmachnetworks.zendesk.com%2Fagent%2Ffilters%2F360205088072")
-            page.goto("https://machnetworks.zendesk.com/auth/v2/login/signin?return_to=https%3A%2F%2Fmachnetworks.zendesk.com%2Fagent%2Ffilters%2F360205088072&theme=hc&locale=1&brand_id=360005574752&auth_origin=360005574752%2Cfalse%2Ctrue&role=agent")
-            page.get_by_label("Email").click()
-            page.get_by_label("Email").fill("esanchez@machnetworks.com")
-            page.get_by_label("Password").click()
-            page.get_by_label("Password").fill("Esm522@@Zendesk")
-            page.get_by_role("button", name="Sign in").click()
-
-       # Adding a new ticket
-            page.locator(
-                "[data-test-id=\"header-toolbar-add-menu-button\"]").hover()
-            page.locator(
-                "[data-test-id=\"header-toolbar-add-menu-new-ticket\"]").click()
-
-       # Create a new ticket from template
-            page.locator("[data-test-id=\"omni-header-subject\"]").click()
-            page.locator(
-                "[data-test-id=\"omni-header-subject\"]").fill(Ticket_title)
-            page.locator(
-                "[data-test-id=\"ticket-footer-macro-menu-autocomplete-input\"]").get_by_text("Apply macro").click()
-
-            
-            
-            if row[0].startswith('Site Survey'):
-                page.locator("[data-test-id=\"ticket-footer-macro-menu-autocomplete-input\"]").get_by_label("Apply macro autocomplete menu").click()
-                page.locator("[data-test-id=\"ticket-footer-macro-menu-autocomplete-input\"]").get_by_label("Apply macro autocomplete menu").fill("Site")
-                page.locator("[data-test-id=\"ticket-footer-macro-menu-autocomplete-input\"]").get_by_label("Apply macro autocomplete menu").fill("Site Survey")
+                page.goto("https://machnetworks.zendesk.com/access/unauthenticated?return_to=https%3A%2F%2Fmachnetworks.zendesk.com%2Fagent%2Ffilters%2F360205088072")
+                page.goto("https://machnetworks.zendesk.com/auth/v2/login/signin?return_to=https%3A%2F%2Fmachnetworks.zendesk.com%2Fagent%2Ffilters%2F360205088072&theme=hc&locale=1&brand_id=360005574752&auth_origin=360005574752%2Cfalse%2Ctrue&role=agent")
+                page.get_by_label("Email").click()
+                page.get_by_label("Email").fill("esanchez@machnetworks.com")
+                page.get_by_label("Password").click()
+                page.get_by_label("Password").fill("Esm522@@Zendesk")
+                page.get_by_role("button", name="Sign in").click()
                 
-                
-                
-                ###Need to fix this so it can be clicked for the template to spawn on the page, try rerunning the playwright codegen script 
-                page.locator("[data-test-id=\"ticket-footer-macro-item\"]").click()
+                # Open the CSV file for reading
+                with open('Create Site Survey/tickets.csv', newline='') as csvfile:
+                    csvreader = csv.reader(csvfile)
+                    next(csvreader)  # Skip the header row if it exists
+
+                    # Iterate over each row in the CSV file
+                    for row in csvreader:
+                        process_row(row)  # Process the current row
+                        print('Hello from csv reader')
+                        # Adding a new ticket
+                        page.locator(
+                            "[data-test-id=\"header-toolbar-add-menu-button\"]").hover()
+                        page.locator(
+                            "[data-test-id=\"header-toolbar-add-menu-new-ticket\"]").click()
+
+                        # Create a new ticket from template
+                        page.locator("[data-test-id=\"omni-header-subject\"]").click()
+                        page.locator(
+                            "[data-test-id=\"omni-header-subject\"]").fill(Ticket_title)
+                        page.locator(
+                            "[data-test-id=\"ticket-footer-macro-menu-autocomplete-input\"]").get_by_text("Apply macro").click()
+
+                        
+                        
+                        if row[0].startswith('Site Survey'):
+                            page.locator("[data-test-id=\"ticket-footer-macro-menu-autocomplete-input\"]").get_by_label("Apply macro autocomplete menu").click()
+                            page.locator("[data-test-id=\"ticket-footer-macro-menu-autocomplete-input\"]").get_by_label("Apply macro autocomplete menu").fill("Site")
+                            page.locator("[data-test-id=\"ticket-footer-macro-menu-autocomplete-input\"]").get_by_label("Apply macro autocomplete menu").fill("Site Survey")
+                            
+                            time.sleep(1)  # Add a 5-second delay
+                            page.locator("[data-test-id=\"ticket-footer-macro-menu-autocomplete-input\"]").get_by_label("Apply macro autocomplete menu").press("Enter")
 
 
-       # Fill the form
-            page.get_by_text("{Location}").fill(f"Location: {Location}")
-            page.get_by_text("{Carrier}").fill(
-                f"Carrier: {Carrier} | Download: {Download} Mbps | Upload: {Upload} Mbps |")
-            page.get_by_text("{Mount}").fill(f"- {Mount}")
-            page.get_by_text("{FACP}").fill(
-                f"FACP: {Facp} | Primary Number: {Primary} | Secondary Number: {Secondary} |")
-            page.get_by_text("{Burglar}").fill(f"Burglar Panel: {Burglar}")
-            page.get_by_text("{Intercomm}").fill(f"Intercom: {Intercomm}")
+                # Fill the form
+                        page.get_by_text("{Location}").fill(f"Location: {Location}")
+                        page.get_by_text("{Carrier}").fill(
+                            f"Carrier: {Carrier} | Download: {Download} Mbps | Upload: {Upload} Mbps |")
+                        page.get_by_text("{Mount}").fill(f"- {Mount}")
+                        page.get_by_text("{FACP}").fill(
+                            f"FACP: {Facp} | Primary Number: {Primary} | Secondary Number: {Secondary} |")
+                        page.get_by_text("{Burglar}").fill(f"Burglar Panel: {Burglar}")
+                        page.get_by_text("{Intercomm}").fill(f"Intercom: {Intercomm}")
 
-        # Check for number of elevators
-            # And then adjust the Reports Number of Elevators
-            page.get_by_text("{Elevator}").fill(
-                f"{number_of_elevators} Elevator : {Elevator}")
-            page.get_by_text("{Elevator Notes}").fill(
-                f"Elevator Notes: {Elevator_notes}")
+                    # Check for number of elevators
+                        # And then adjust the Reports Number of Elevators
+                        page.get_by_text("{Elevator}").fill(
+                            f"{number_of_elevators} Elevator : {Elevator}")
+                        page.get_by_text("{Elevator Notes}").fill(
+                            f"Elevator Notes: {Elevator_notes}")
 
-            # Continue filling the form
-            page.get_by_text("{Fax}").fill(f"Fax: {Fax}")
-            page.get_by_text("{Total DiDs}").fill(
-                f"Total Number of DiDs: {Total_dids}")
-            page.get_by_text("{Notes}").fill(f"Notes: {Notes}")
+                        # Continue filling the form
+                        page.get_by_text("{Fax}").fill(f"Fax: {Fax}")
+                        page.get_by_text("{Total DiDs}").fill(
+                            f"Total Number of DiDs: {Total_dids}")
+                        page.get_by_text("{Notes}").fill(f"Notes: {Notes}")
 
-       # Submit ticket requester, assignee, and submit ticket
-            page.get_by_placeholder("search name or contact info").fill("er")
-            page.locator(
-                "[data-test-id=\"ticket-system-field-requester-menu\"]").get_by_text("ik Sanchez").click()
-            page.locator(
-                "[data-test-id=\"assignee-field-take-it-button\"]").click()
+                # Submit ticket requester, assignee, and submit ticket
+                        page.get_by_placeholder("search name or contact info").fill("er")
+                        page.locator(
+                            "[data-test-id=\"ticket-system-field-requester-menu\"]").get_by_text("ik Sanchez").click()
+                        page.locator(
+                            "[data-test-id=\"assignee-field-take-it-button\"]").click()
 
-        # # Add followers
-            page.get_by_role("combobox", name="Followers").click()
-            page.get_by_placeholder("search agents").click()
-            page.get_by_placeholder("search agents").fill("ht")
-            page.locator("[data-test-id=\"ticket-fields-collaborators-contact-card\"]").click()
-            page.get_by_role("combobox", name="Followers").fill("chris")
-            page.locator("[data-test-id=\"ticket-fields-collaborators-contact-card\"]").click()
+                    # # Add followers
+                        page.get_by_role("combobox", name="Followers").click()
+                        page.get_by_placeholder("search agents").click()
+                        page.get_by_placeholder("search agents").fill("ht")
+                        page.locator("[data-test-id=\"ticket-fields-collaborators-contact-card\"]").click()
+                        page.get_by_role("combobox", name="Followers").fill("chris")
+                        page.locator("[data-test-id=\"ticket-fields-collaborators-contact-card\"]").click()
 
-        # Get the date to follow up on
-            page.get_by_placeholder("e.g. October 1, 2008").click()
-            page.get_by_placeholder("e.g. October 1, 2008").fill(
-                followup_date_string)
+                    # Get the date to follow up on
+                        # page.get_by_placeholder("e.g. October 1, 2008").click()
+                        # page.get_by_placeholder("e.g. October 1, 2008").fill(
+                        #     followup_date_string)
 
-        # Ticket Priority
-            page.locator(
-                "[data-test-id=\"ticket-fields-priority-select\"]").click()
-            page.get_by_role("option", name="Normal").click()
+                    # Ticket Priority
+                        page.locator(
+                            "[data-test-id=\"ticket-fields-priority-select\"]").click()
+                        page.get_by_role("option", name="Normal").click()
 
-        # Ticket Type
-            page.locator(
-                "[data-test-id=\"ticket-form-field-dropdown-button\"] svg").click()
-            page.get_by_role("option", name="Field Install").click()
-            page.locator(
-                "[data-test-id=\"ticket-form-field-dropdown-field-8323312621837\"] svg").click()
-            page.get_by_text("Site Survey WO").click()
+                    # Ticket Type
+                        page.locator(
+                            "[data-test-id=\"ticket-form-field-dropdown-button\"] svg").click()
+                        page.get_by_role("option", name="Field Install").click()
+                        page.locator(
+                            "[data-test-id=\"ticket-form-field-dropdown-field-8323312621837\"] svg").click()
+                        page.get_by_text("Site Survey WO").click()
 
-        # Ticket Resolution
-            page.locator(
-                "[data-test-id=\"ticket-fields-multiline-field\"]").click()
-            page.locator("[data-test-id=\"ticket-fields-multiline-field\"]").fill(
-                "Site survey completed. Please see the attached for the site survey results.")
+                    # Ticket Resolution
+                        page.locator(
+                            "[data-test-id=\"ticket-fields-multiline-field\"]").click()
+                        page.locator("[data-test-id=\"ticket-fields-multiline-field\"]").fill(
+                            "Site survey completed. Please see the attached for the site survey results.")
 
-            page.locator(
-                "[data-test-id=\"ticket-composer-toolbar-link-button\"]").click()
-            page.locator(
-                "[data-test-id=\"link-modal-link-input\"]").fill(Fieldnation_Workorder_Link)
-            page.locator("[data-test-id=\"link-modal-text-input\"]").click()
-            page.locator(
-                "[data-test-id=\"link-modal-text-input\"]").fill("\n Field Nation Work Order")
-            page.locator("[data-test-id=\"link-modal-add-button\"]").click()
+                        page.locator(
+                            "[data-test-id=\"ticket-composer-toolbar-link-button\"]").click()
+                        page.locator(
+                            "[data-test-id=\"link-modal-link-input\"]").fill(Fieldnation_Workorder_Link)
+                        page.locator("[data-test-id=\"link-modal-text-input\"]").click()
+                        page.locator(
+                            "[data-test-id=\"link-modal-text-input\"]").fill("\n Field Nation Work Order")
+                        page.locator("[data-test-id=\"link-modal-add-button\"]").click()
 
-        # Submit Ticket
-            page.locator(
-                "[data-test-id=\"submit_button-menu-button\"]").hover()
-            page.locator(
-                "[data-test-id=\"submit_button-menu-button\"]").click()
-            page.get_by_text("Submit as Pending").click()
+                    # Submit Ticket
+                        page.locator(
+                            "[data-test-id=\"submit_button-menu-button\"]").hover()
+                        page.locator(
+                            "[data-test-id=\"submit_button-menu-button\"]").click()
+                        time.sleep(5)  # Add a 5-second delay
+                        page.get_by_text("Submit as Pending").click()
 
-        # # Return to the views page
-            # page.locator("[data-test-id=\"views_icon\"]").click()
+                    # # # Return to the views page
+                    #     page.locator("[data-test-id=\"views_icon\"]").click()
+                    #     time.sleep(3)  # Add a 5-second delay
 
 
-       # Close the browser
-            context.close()
-            browser.close()
+            finally:
+        # Close the browser
+                context.close()
+                browser.close()
 
 
 with sync_playwright() as playwright:
---
+++ ImageResize/main.py
@@ -0,0 +1,32 @@
+from PIL import Image
+import os
+
+# Specify the input and output directories
+input_directory = "C:/Users/eriks/Downloads/SiteImages"
+output_directory = "C:/Users/eriks/Downloads/SiteImages/Processed"
+
+# Create the output directory if it doesn't exist
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+    
+    
+# Iterate through the files in the input directory
+for filename in os.listdir(input_directory):
+    # Check if the file is an image (you can add more image formats as needed)
+    if filename.endswith((".jpg", ".jpeg", ".png", ".gif")):
+        # Open the image using Pillow
+        with Image.open(os.path.join(input_directory, filename)) as img:
+            # Calculate the new width and height (60% of the original size)
+            width, height = img.size
+            new_width = int(width * 0.6)
+            new_height = int(height * 0.6)
+            
+            # Resize the image to the new dimensions using BILINEAR filter
+            resized_img = img.resize((new_width, new_height), Image.BILINEAR)
+            
+            # Save the resized image to the output directory
+            output_path = os.path.join(output_directory, filename)
+            resized_img.save(output_path)
+            print(f"Resized {filename} to {new_width}x{new_height} and saved to {output_path}")
+
+print("All images resized.")
